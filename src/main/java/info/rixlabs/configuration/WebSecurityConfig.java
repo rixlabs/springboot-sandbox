cf92e01bd78915aed61352448d89e2c1ef58226e
Riccardo Causo <riccardo.causo@lugano.ch>
2016-05-19 17:58:47 +0200

    added explicit logout logic, it does the default thing but is written down

--- src/main/java/info/rixlabs/configuration/WebSecurityConfig.java
+++ src/main/java/info/rixlabs/configuration/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import info.rixlabs.models.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by riccardo.causo on 12.05.2016.
@@ -23,15 +25,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //For me this config can become huge and complicated, can we split it????
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers(HttpMethod.POST,"/auth/**").permitAll()
                     .anyRequest().fullyAuthenticated()
                     .and()
                 .httpBasic()
                 .and()
-                .csrf();
+                .csrf().disable()
+                .logout()
+                    .logoutUrl("/logout")
+                    .invalidateHttpSession(true);
     }
 
     @Autowired
